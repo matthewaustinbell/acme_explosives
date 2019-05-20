f443d3672367497a038be12fc20eeb15a67329de
Matt Bell <matthewaustinbell@gmail.com>
2019-05-19 21:44:45 -0500

    wip

--- src/javascripts/helpers/data/typesData.js
+++ src/javascripts/helpers/data/typesData.js
@@ -1,5 +1,33 @@
 import axios from 'axios';
 
-const loadCategories = () => axios.get('../db/boards.json');
+/* creates the new promise */
+/* gets the data from types.json *
+/* assigns resp.data.types to variable allTypes */
+/* uses filter method with conditional */
 
-export default { loadCategories };
+const loadTypesForCategorie = categorieId => new Promise((resolve, reject) => {
+  axios.get('../db/types.json')
+    .then((resp) => {
+      const allTypes = resp.data.types;
+      const matchingTypes = allTypes.filter(type => type.categoryId === categorieId);
+      resolve(matchingTypes);
+    })
+    .catch(err => reject(err));
+});
+
+const getTypesForEachCategorie = categories => new Promise((resolve, reject) => {
+  axios.get('../db/types.json')
+    .then((resp) => {
+      const { types } = resp.data;
+      const categoriesWithTypes = categories.map((categorie) => {
+        const newCategorie = categorie;
+        const matchingTypes = types.filter(type => type.categorieId === categorie.id);
+        newCategorie.types = matchingTypes;
+        return newCategorie;
+      });
+      resolve(categoriesWithTypes);
+    })
+    .catch(err => reject(err));
+});
+
+export default { loadTypesForCategorie, getTypesForEachCategorie };
